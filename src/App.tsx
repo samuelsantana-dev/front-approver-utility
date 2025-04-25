b1856e5b66cff4a0e1c9a0e530085bbd6b5bab75
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-04-25 10:26:43 -0300

    Exclusão de menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Box } from '@mui/material'
 import './App.css'
-import { ModelMenu } from './components/menu/menu'
 import { MessageTemplatePreview } from './components/messageTemplateCreator/messageTemplateCreator'
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
 
   return (
     <Box sx={{ margin: 'auto', display: 'flex', gap: 3 }}>
-      <ModelMenu />
       <MessageTemplatePreview />
     </Box>
   )
